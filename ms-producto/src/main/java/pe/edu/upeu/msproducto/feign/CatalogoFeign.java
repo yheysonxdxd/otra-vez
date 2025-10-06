7436a66eacd5856a23310d2116457ea4a62d4f83
yheysonxdxd <yheysoncasas@gmail.com>
2025-10-06 14:31:44 -0500

    dsndklasd

--- ms-producto/src/main/java/pe/edu/upeu/msproducto/feign/CatalogoFeign.java
+++ ms-producto/src/main/java/pe/edu/upeu/msproducto/feign/CatalogoFeign.java
@@ -1,14 +1,22 @@
 package pe.edu.upeu.msproducto.feign;
 
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import pe.edu.upeu.msproducto.dto.CatagoriaDto;
 
-import java.util.Optional;
-
 @FeignClient(name = "ms-catalogo", path = "/categoria")
 public interface CatalogoFeign {
     @GetMapping("/{id}")
+    @CircuitBreaker(name = "categoriaListarPorIdCB", fallbackMethod = "fallbackCategoria")
     public CatagoriaDto buscarPorId(@PathVariable Integer id);
+
+    default CatagoriaDto fallbackCategoria(Integer id, Exception e) {
+        CatagoriaDto catagoriaDto = new CatagoriaDto();
+        catagoriaDto.setId(9000000);
+        catagoriaDto.setNombre("Servicio Categoria no disponible");
+        return catagoriaDto;
+    }
+
 }
